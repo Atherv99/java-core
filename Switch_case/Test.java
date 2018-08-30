4c2b2f7163fa398819ba35b463b516efb218c9bb
Ashwani Jaiswal <42098141+Atherv99@users.noreply.github.com>
2018-08-30 19:10:43 +0530

    Add files via upload

---
+++ Switch_case/Test.java
@@ -0,0 +1,27 @@
+package Switch_case;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		switch(i)
+		{
+		case 5:
+		{
+			System.out.println("Five");
+			break;
+		}
+		case 10:
+		{
+			System.out.println("ten");
+			break;
+		}
+		case 15:
+			System.out.println("Fifteen");
+			break;
+		}
+
+	}
+
+}
