56deb083f0d09fb60eef58e540c43e1889a26001
Ashwani Jaiswal <42098141+Atherv99@users.noreply.github.com>
2018-08-30 19:07:26 +0530

    Add files via upload

---
+++ Do_while_loop/Test.java
@@ -0,0 +1,20 @@
+package Do_while_loop;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		do
+		{
+			System.out.println(i);
+			i++;
+		}
+		while(i<10);
+		{
+			
+		}
+
+	}
+
+}
---
+++ Do_while_loop/Test1.java
@@ -0,0 +1,22 @@
+package Do_while_loop;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		System.out.println("Before loop");
+		
+		do
+		{
+			System.out.println("Inside loop");
+			i++;
+		}
+		while(true);
+		
+		//System.out.println("After loop");
+
+	}
+
+}
+//output: compilation Error when we using 17 line statement
