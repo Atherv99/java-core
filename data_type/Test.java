9bca1924e8eafc20477a9d877e2f09487d3b7fd5
Ashwani Jaiswal <42098141+Atherv99@users.noreply.github.com>
2018-08-30 19:05:37 +0530

    Add files via upload

---
+++ data_type/Test.java
@@ -0,0 +1,13 @@
+package data_type;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=100;
+		long j=x;
+		System.out.println(j);
+
+	}
+
+}
---
+++ data_type/Test1.java
@@ -0,0 +1,13 @@
+package data_type; //it is Explicit down casting
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long x=100;
+		int j=(int)x;
+		System.out.println(j);
+
+	}
+
+}
