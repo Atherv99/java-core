c6ddd3e8606a1f2c0c5c287c32388578989566e8
Ashwani Jaiswal <42098141+Atherv99@users.noreply.github.com>
2018-08-30 19:09:56 +0530

    Add files via upload

---
+++ local_variable/A.java
@@ -0,0 +1,20 @@
+package local_variable;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k=50;
+		System.out.println("Hello "+k);
+		A t=new A();
+		t.test();
+		
+
+	}
+	public void test()
+	{
+		int a=100;
+		System.out.println(a);
+	}
+
+}
