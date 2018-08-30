98fbeea78fc513d66c4ca7da88bbae6a6efd2f66
Ashwani Jaiswal <42098141+Atherv99@users.noreply.github.com>
2018-08-30 19:08:31 +0530

    Add files via upload

---
+++ Instance_variable/A.java
@@ -0,0 +1,13 @@
+package Instance_variable;
+
+public class A {
+	
+	int i=100;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A a1=new A();
+		System.out.println(a1.i);
+
+	}
+
+}
