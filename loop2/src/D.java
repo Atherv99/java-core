640f05ef7ddfa593a64395d0e1e4df2b83f6edad
Ashwani Jaiswal <42098141+Atherv99@users.noreply.github.com>
2018-08-31 17:50:24 +0530

    Add files via upload

---
+++ loop2/src/D.java
@@ -0,0 +1,21 @@
+class D{
+
+	public static void main(String args[]){
+	
+		int i = 1;
+		switch(i){
+		
+			case 1:
+				System.out.println("from 1");
+				System.out.println("from 111");
+			case 2:
+				System.out.println("from 2");
+				System.out.println("from 222");
+			case 3:
+				System.out.println("from 3");
+				System.out.println("from 333");
+			
+		}
+		System.out.println("end of  the main");
+	}
+}
